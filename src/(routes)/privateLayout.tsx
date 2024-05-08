56cf773dbfded2e1998fe01c6c9ae44da3680c2d
Kim Kangmin <rlarkdals1202@gmail.com>
2024-05-08 21:02:32 +0900

    fix: 관리자 페이지 안들어가지는 버그 수정

--- src/(routes)/privateLayout.tsx
+++ src/(routes)/privateLayout.tsx
@@ -34,6 +34,7 @@ export default function PrivateLayout() {
                 >
                     <Outlet />
                 </Suspense>
+                <Outlet />
             </ErrorBoundary>
         </>
     );
