d7a9718dc4f8a0c69558a455590ebd84b92aeeb1
wonza-hub <70367717+wonza-hub@users.noreply.github.com>
2025-10-15 17:10:28 +0900

    fix: merge 충돌 시 잘못 들어간 라인 삭제

--- src/(routes)/privateLayout.tsx
+++ src/(routes)/privateLayout.tsx
@@ -34,7 +34,6 @@ export default function PrivateLayout() {
                 >
                     <Outlet />
                 </Suspense>
-                <Outlet />
             </ErrorBoundary>
         </>
     );
